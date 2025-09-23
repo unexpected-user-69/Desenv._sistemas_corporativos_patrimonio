5da89b81b9e298f805117461852d8e2cc90b5ef2
Diogo <diogocrespi@alunos.utfpr.edu.br>
2025-09-23 15:55:34 -0300

    fix(lint): remover create/merge dinâmico e usar atribuições explícitas no UsersServic

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,23 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
+import * as http from 'http';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let httpServer: http.Server;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
+
+    // Correção: Asserção de tipo para resolver o erro de atribuição insegura.
+    httpServer = app.getHttpServer() as http.Server;
   });
 
-  it('/ (GET)', async () => {
-    const response = await request(app.getHttpServer()).get('/');
-    expect(response.status).toBe(200);
-    expect(response.text).toBe('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('/ (GET)', () => {
+    return request(httpServer).get('/').expect(200).expect('Hello World!');
   });
 });
